41750940f25c0ae13b09091210c1955414e8fc02
Michael GENA <mgena@nuxeo.com>
2015-12-10 15:37:25 +0100

    Subscription implementation

--- nuxeo-unify-php-plugin/src/main/yo/app/scripts/app.js
+++ nuxeo-unify-php-plugin/src/main/yo/app/scripts/app.js
@@ -20,12 +20,12 @@ angular
   .service('nuxeoClient', function(nuxeoClientFactory) {
 	  var client = nuxeoClientFactory({
         // Comment lines below to use prod instance
-        /*baseURL: 'http://localhost:8080/nuxeo',
+        baseURL: 'http://localhost:8080/nuxeo',
         'auth': {
           'method':'basic',
           'username':'Administrator',
           'password':'Administrator'
-        }*/
+        }
   		});
       // fetch all schemas
       client.schema('*');
--- nuxeo-unify-php-plugin/src/main/yo/app/scripts/controllers/main.js
+++ nuxeo-unify-php-plugin/src/main/yo/app/scripts/controllers/main.js
@@ -16,7 +16,34 @@ angular.module('frontApp')
   $scope.sortType     = 'attributes.product_version_name'; // set the default sort type
   $scope.sortReverse  = false;  // set the default sort order
   $scope.searchProduct   = '';     // set the default search/filter term
+  $scope.selectedAll = false;
 
+  $scope.checkAll = function () {
+       if ($scope.selectedAll) {
+          $scope.selectedAll = true;
+          $("#subscription").removeClass("disabled");
+       } else {
+          $scope.selectedAll = false;
+          $("#subscription").addClass("disabled");
+       }
+       angular.forEach($scope.products, function (product) {
+           product.selected = $scope.selectedAll;
+       });
+   };
+
+     $scope.check = function () {
+          var hasAtLeastOneChecked = false;
+          angular.forEach($scope.products, function (product) {
+              if (product.selected) {
+                hasAtLeastOneChecked = true;
+              }
+          });
+          if (hasAtLeastOneChecked) {
+             $("#subscription").removeClass("disabled");
+          } else {
+             $("#subscription").addClass("disabled");
+          }          
+      };
 
   // Main callback
   function callbackGPA(result) {
